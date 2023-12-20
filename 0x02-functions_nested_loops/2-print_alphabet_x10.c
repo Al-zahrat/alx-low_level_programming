77dc69335dcfebe47ae8f1cb1e1bb764f631358d
Ezzahra <ezzahra.elidrissi1@gmail.com>
2023-12-21 00:26:10 +0100

    functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints 10 times the alphabet
+*/
+void print_alphabet_x10(void)
+{
+	int i = 97;
+	int j = 0;
+
+	while (j < 10)
+	{
+		for (; i <= 122; i++)
+			_putchar(i);
+		_putchar('\n');
+		j++;
+	}
+}
