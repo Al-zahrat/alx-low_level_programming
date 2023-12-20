0b18e8fbb707c9a6fef09caf94b1f7d5ae315295
Ezzahra <ezzahra.elidrissi1@gmail.com>
2023-12-20 21:10:38 +0100

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - function that prints the alphabet, in lowercase
+*/
+void print_alphabet(void)
+{
+	int i = 97;
+
+	for (; i <= 122; i++)
+		_putchar(i);
+	_putchar('\n');
+}
