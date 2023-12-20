d3729a2abcc58b05776485ecf079f1a575031a34
Ezzahra <ezzahra.elidrissi1@gmail.com>
2023-12-21 00:51:50 +0100

    functions

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/** _islower -  function that checks for lowercase
+ * @c: parameter
+ * Return: 1 if success 0 otherwise
+*/
+
+int _islower(int c)
+{
+	if (c <= 122 && c >= 97)
+		return (1);
+	return (0);
+}
