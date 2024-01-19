6a93dc1308a13fdedc10259105a94344ede0ae1f
zahra <ezzahraelidrissi1@gmail.com>
2024-01-19 23:29:59 +0100

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of string
+ * @s: parameter
+ * Return: integer
+*/
+
+int _strlen(char *s)
+{
+	char x[] = s;
+	int i = 0;
+
+	while (x[i] != '\0')
+	{
+		i += 1;
+	}
+	return (i);
+}
