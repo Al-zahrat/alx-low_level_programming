fdecf0f79a1a0a71fd2bf0f1da74c9133c5dd88d
zahra <ezzahraelidrissi1@gmail.com>
2024-01-20 02:13:24 +0100

    _puts

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - function to reverse string
+ * @s: parameter
+*/
+void print_rev(char *s)
+{
+	int c = _strlen(char *s);
+
+	for (; c > 0; c++)
+		_putchar(s[c]);
+
+	_putchar('\n');
+}
