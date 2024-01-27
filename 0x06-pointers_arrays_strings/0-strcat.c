0aa8c2411eb5595be169076b93cf26bbebf6db02
zahra <ezzahraelidrissi1@gmail.com>
2024-01-27 22:07:33 +0100

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,12 +12,12 @@ char *_strcat(char *dest, char *src)
 
 	while (dest[i] != '\0')
 	{
-		i = i + 1;
+		i++;
 	}
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		i = i + 1;
+		i++;
 		j++;
 	}
 	dest[i] = '\0';
