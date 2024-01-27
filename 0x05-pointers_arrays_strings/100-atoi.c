1b9fc04ce89d49927388b109897ec4db8cb38c0e
zahra <ezzahraelidrissi1@gmail.com>
2024-01-27 18:20:50 +0100

    rev

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,14 +10,18 @@ int _atoi(char *s)
 	int i = 0;
 	int sign = 1;
 
-	while (s[i] != '\0')
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9') && s[i] != '-')
 	{
-		if (s[i] == '-')
-			sign = -1;
-		if (s[i] >= 48 && s[i] <= 57)
-		{
-			x = (x * 10) + (s[i] - '0');
-		}
+		i++;
+	}
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (s[i] >= 48 && s[i] <= 57)
+	{
+		x = (x * 10) + (s[i] - '0');
 		i++;
 	}
 	return (x * sign);
