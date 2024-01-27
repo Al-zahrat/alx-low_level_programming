d5364854009f1deba541e309fc6cf7697cbe40f9
zahra <ezzahraelidrissi1@gmail.com>
2024-01-27 16:43:00 +0100

    rev

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,12 +7,13 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0, j = 0;
+	int i = 0;
+	int j = 0;
 
-	while (dest[i] != '\0')
+	/*while (dest[i] != '\0')
 	{
 		i = i + 1;
-	}
+	}*/
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
