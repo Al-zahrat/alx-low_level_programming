42514ed9928565385c073d3e856180d6ed2f03ca
zahra <ezzahraelidrissi1@gmail.com>
2024-01-27 13:54:09 +0100

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - function that print n number of array
+ * @a: parameter
+ * @n: parameter
+*/
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		if (i == n - 1)
+		{
+			_putchar(a[i]);
+		}
+		else
+			printf("%d, ", a[i]);
+	}
+	_putchar('\n');
+}
