528cd806d133d2368c003655f7ea4f2f24712968
zahra <ezzahraelidrissi1@gmail.com>
2024-01-27 17:37:12 +0100

    rev

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _atoi - function to convert string to int
+ * @s: parameter
+ * Return: int
+*/
+int _atoi(char *s)
+{
+	int x = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			x = (x * 10) + s[i];
+			if (s[i - 1] == 45)
+			{
+				x = -x;
+			}
+		}
+	}
+	return (x);
+}
