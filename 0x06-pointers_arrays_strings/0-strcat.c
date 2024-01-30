b55031974adb7edc5a8695946393cd605238f4f7
zahra <ezzahraelidrissi1@gmail.com>
2024-01-30 20:09:48 +0100

    res

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,12 +7,14 @@
 */
 char *_strcat(char *dest, char *src)
 {
+	char *res = dest;
 	int i = 0;
 	int j = 0;
 
 
 	while (dest[i] != '\0')
 	{
+		i++;
 	}
 
 	while (src[j] != '\0')
@@ -22,5 +24,5 @@ char *_strcat(char *dest, char *src)
 		j++;
 	}
 	dest[i + j] = '\0';
-	return (dest);
+	return (res);
 }
