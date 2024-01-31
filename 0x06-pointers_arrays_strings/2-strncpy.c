a590f80ed87c0611844ccdcc84c9ab85451408bd
zahra <ezzahraelidrissi1@gmail.com>
2024-02-01 00:27:36 +0100

    strcat

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - function that copies a string.
+ * @dest: parameter
+ * @src: parameter
+ * @n:  parameter
+ * Return: pointer
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	while (dest[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i >= n)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
