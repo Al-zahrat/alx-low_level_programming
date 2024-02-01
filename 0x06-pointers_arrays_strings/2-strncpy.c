dd6eb4a1323d2f245fccbb206ee444e661ee2eb8
zahra <ezzahraelidrissi1@gmail.com>
2024-02-01 01:07:34 +0100

    strcat

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,14 +11,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (dest[i] != '\0' && i < n)
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	if (i >= n)
+	while (i >= n)
 	{
 		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
