579c75abc33d181761590dac3d283596188931a7
zahra <ezzahraelidrissi1@gmail.com>
2024-02-05 11:31:19 +0100

    upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * string_toupper - function that changes a string to uppercase
- * @s: parameter
+ * @p: parameter
  * Return: string
 */
 char *string_toupper(char *p)
 {
 	char *s = p;
+
 	while (*s)
 	{
 		if (*s <= 122 && *s >= 97)
