1abf2e21238dee020b9f5849a7d09c6672058588
zahra <ezzahraelidrissi1@gmail.com>
2024-02-14 22:51:00 +0100

    cap_string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,21 @@
 char *leet(char *s)
 {
 	char *str = s;
+	char p[6][4] = {{'a', 'A', '4'},
+		{'e', 'E', '3'},
+		{'o', 'O', '0'},
+		{'t', 'T', '7'},
+		{'l', 'L', '1'}};
+	int i;
 
 	while (*str)
 	{
-		if (*str == 'a' || *str == 'A')
-			*str = '4';
-		else if (*str == 'e' || *str == 'E')
-			*str = '3';
-		else if (*str == 'o' || *str == 'O')
-			*str = '0';
-		else if (*str == 't' || *str == 'T')
-			*str = '7';
-		else if (*str == 'l' || *str == 'L')
-			*str = '1';
-
+		i = 0;
+		for (; i <= 4; i++)
+		{
+			if (*str == p[i][0] || *str == p[i][1])
+				*str = p[i][2];
+		}
 		str++;
 	}
 	return (s);
