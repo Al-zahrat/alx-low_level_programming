17e88e313a964bbb0196b05922102deeef3fa990
zahra <ezzahraelidrissi1@gmail.com>
2024-02-14 14:25:08 +0100

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,5 @@
 #include "main.h"
-/**
- * alphaL - function that check if char is lowercase
- * @i: parameter
- * Return: integer
-*/
-int alphaL(char i)
-{
-	if (i <= 'z' && i >= 'a')
-		return (1);
-	else if (i >= 'A' && i <= 'Z')
-		return (2);
-	else
-		return (0);
-}
+
 /**
  * cap_string - function that capitalizes all words
  * @s: parameter
@@ -35,18 +22,14 @@ char *cap_string(char *s)
 		{
 			x = 1;
 		}
-		else if (x == 1 && alphaL(*p) == 1)
+		else if (x == 1 && (*p >= 'a' && *p <= 'z'))
 		{
 			*p -= 32;
 			x = 0;
 		}
-		else if (x && alphaL(*p) == 2)
+		else
 		{
 			x = 0;
-		}
-		else if (x == 0 && alphaL(*p) == 2)
-		{
-			*p += 32;
 		}
 		++p;
 	}
