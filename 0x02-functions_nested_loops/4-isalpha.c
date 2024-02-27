ca757427001783f205a79e6a8bc98e23f62e4a70
zahra <ezzahraelidrissi1@gmail.com>
2024-02-27 22:28:27 +0100

    prog

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - function to check if c is alpha
+ * @c: parameter
+ * Return: 1 if success 0 Otherwise
+*/
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
