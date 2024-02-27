df0d1e090efc8d4a83c6b6bc2e5c0b1434cfe07a
zahra <ezzahraelidrissi1@gmail.com>
2024-02-28 00:41:32 +0100

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,15 +16,13 @@ void jack_bauer(void)
 				{
 					if (i == 2 && j > 3)
 						break;
-					else
-					{
-						_putchar(i + '0');
-						_putchar(j + '0');
-						_putchar(':');
-						_putchar(k + '0');
-						_putchar(l + '0');
-						_putchar('\n');
-					}
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+
 				}
 			}
 		}
