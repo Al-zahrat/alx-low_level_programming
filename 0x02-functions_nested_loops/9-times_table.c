cacca08f379a3a17e454ac375c1fae111988c1a1
zahra <ezzahraelidrissi1@gmail.com>
2024-02-28 02:00:13 +0100

    24

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * times_table - print 9 times table
+*/
+void times_table(void)
+{
+	int i, j, t;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			t = i * j;
+			_putchar(t + '0');
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar('  ';
+			}
+		}
+		_putchar('\n');
+	}
+}
