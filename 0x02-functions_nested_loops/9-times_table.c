6dc581044c698d6815bcc61c0e80c4b529ab948a
zahra <ezzahraelidrissi1@gmail.com>
2024-02-28 07:27:14 +0100

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,20 +4,31 @@
 */
 void times_table(void)
 {
-	int i, j, t;
+	int i, j, t, p, x = 0;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			t = i * j;
-			_putchar(t + '0');
+			if (t < 10)
+			{
+				_putchar(t + '0');
+				x = 2;
+			}
+			else
+			{
+				p = t / 10;
+				_putchar(p + '0');
+				p = t % 10;
+				_putchar(p + '0');
+			}
 			if (j < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (j < 9 && t < 10)
+			if (j < 9 && x == 2)
 				_putchar(' ');
 		}
 		_putchar('\n');
