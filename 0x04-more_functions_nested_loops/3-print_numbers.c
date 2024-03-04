41ef7206f330498344ccfd3acb8984048420db40
zahra <ezzahraelidrissi1@gmail.com>
2024-03-04 18:53:14 +0100

    more functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints the numbers
+*/
+void print_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		_putchar(i + '0');
+	}
+}
