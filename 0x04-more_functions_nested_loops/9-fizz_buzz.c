e7a238ec407df0d9bc32e5e9f06e456411954c67
zahra <ezzahraelidrissi1@gmail.com>
2024-03-05 15:11:53 +0100

    more functions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: program to print fizz buzz
+ * Return: (0) success
+*/
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i < 100)
+			_putchar(' ');
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
