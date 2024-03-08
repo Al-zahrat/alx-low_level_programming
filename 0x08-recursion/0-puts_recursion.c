b3da03845f735279478c0a0963d52e93880ea1ae
zahra <ezzahraelidrissi1@gmail.com>
2024-03-08 21:25:07 +0100

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that print string
+ * @s: parameter
+*/
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s--;
+	if (*s != '\0')
+		_puts_recursion(s);
+}
