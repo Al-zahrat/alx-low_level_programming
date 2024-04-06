54489cd551d99b62dfe630cbc17145adf0904992
zahra <ezzahraelidrissi1@gmail.com>
2024-04-06 08:21:40 +0000

    argc-argv

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strdup - function to newly allocate space
+ * @str: string
+ * Return:pointer to a new string
+*/
+char *_strdup(char *str)
+{
+
+	if (str == NULL)
+		return (NULL);
+}
