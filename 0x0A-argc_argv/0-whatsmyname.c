1a728790ed8cd6dab4595b5b1c2489bedb76ca18
zahra <ezzahraelidrissi1@gmail.com>
2024-04-18 08:05:09 +0100

    whasmaname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#define UNUSED(x) void(x)
 /**
  * main - Entry point
  * Description: program to print  the file name
@@ -8,9 +7,8 @@
  * @argv: parameter
  * Return: 0 success
 */
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char **argv)
 {
-	UNUSED(argc);
 	printf("%s\n", argv[0]);
 	return (0);
 }
