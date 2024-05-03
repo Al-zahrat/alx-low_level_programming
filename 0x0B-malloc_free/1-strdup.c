85fa1b0180d5c20e273dcde369013d31bb3bdec8
root <root@iPavilion>
2024-05-03 18:12:28 +0000

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,7 +6,21 @@
 */
 char *_strdup(char *str)
 {
+	char *st;
+	int count;
 
 	if (str == NULL)
 		return (NULL);
+	for (count = 0; str[count] != '\0'; count++)
+		count++;
+	count++;
+
+	st = malloc(sizeof(char) * count);
+	count = 0;
+	while (str[count] != '\0')
+	{
+		st[count] = str[count];
+		count++;
+	}
+	return (st);
 }
