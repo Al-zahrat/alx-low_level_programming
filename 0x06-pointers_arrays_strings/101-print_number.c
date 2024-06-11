6003d03afa8c70ca6dbf15cf813ffbb0db342bfd
ezzahra <sindyzahra26@gmail.com>
2024-06-11 13:19:05 +0000

    101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,55 +1,26 @@
 #include "main.h"
-
-/**
- * rev - function that reverse the number
- * @x: parameter
- * Return: integer
-*/
-int rev(int x)
-{
-	int y = 0;
-	int sign = -1;
-
-	if (x == 0)
-	{
-		return (x);
-	}
-	if (x < 0)
-	{
-		x = -x;
-	}
-	else
-	{
-		sign = 1;
-	}
-	while (x != 0)
-	{
-		y = (y * 10) + (x % 10);
-		x = x / 10;
-	}
-	return (y * sign);
-}
 /**
  * print_number - function to print integer
  * @n: parameter
 */
 void print_number(int n)
 {
-	unsigned int m;
-	int d = rev(n);
+	unsigned int d;
 
-	if (d < 0)
+	d = n;
+	if (n < 0)
 	{
 		_putchar('-');
-		d = d * (-1);
+		d = -n;
 	}
 	if (d == 0)
-		_putchar('0');
-
-	while (d != 0)
 	{
-		m = d % 10;
-		_putchar(m + '0');
-		d = d / 10;
+		_putchar(d + '0');
+		return;
 	}
+	if ((d / 10) != 0)
+	{
+		print_number(d / 10);
+	}
+	_putchar((d % 10) + '0');
 }
